07a9ef2b58f70e8ca72d52b0b7779cdddfce157a
Martin Renters <martin@teckelworks.com>
2019-11-22 14:04:40 -0500

    Prep for release 2.0.0

--- dfpython/datafax/datafax/__init__.py
+++ dfpython/datafax/datafax/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = '1.2.0'
+__version__ = '2.0.0'
 __VERSION__ = __version__
 
 from datafax.study import Study
--- sigtrack/main.c
+++ sigtrack/main.c
@@ -29,7 +29,7 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#define VERSION	"1.2.0"
+#define VERSION	"2.0.0"
 
 int yyparse(void);
 int get_config_error_cnt(void);
